5b6a6e3b1494a551662c4c6a410221818fd3f954
Maxwell Warren <mwarre28@uncc.edu>
2016-03-01 18:03:26 -0500

    Menu progress and first attempts at First-person camera controls

---
+++ NEW GAME/Work In Progress/Assets/MenuControllerScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuControllerScript : MonoBehaviour {
+
+    public int CurrentMenu = 0;
+    public Canvas[] MenuCanvases;
+	
+    void Update()
+    {
+        foreach(Canvas c in MenuCanvases)
+            c.enabled = false;
+
+        MenuCanvases[CurrentMenu].enabled = true;
+    }
+	// Update is called once per frame
+	public void SetMenu (int sel) {
+        if (sel != CurrentMenu)
+        CurrentMenu = sel;
+	}
+
+    public void LoadLevel(string level) {
+        SceneManager.LoadScene(level);
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}
---
+++ NEW GAME/Work In Progress/Assets/MoveAndLook.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveAndLook : MonoBehaviour
+{
+
+    public float LookSensitivity = .02f;
+    private Vector3 old, cur, left, right, up, down;
+
+    void Start()
+    {
+        cur = Input.mousePosition;
+        old = Vector3.zero;
+        left = this.gameObject.transform.rotation.eulerAngles + Vector3.left;
+        right = this.gameObject.transform.rotation.eulerAngles + Vector3.right;
+        up = this.gameObject.transform.rotation.eulerAngles + Vector3.up;
+        down = this.gameObject.transform.rotation.eulerAngles + Vector3.down;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Add Camera Rotation Code Here
+        /***********/
+        //
+
+        if (Input.GetKey(KeyCode.W))
+            this.gameObject.transform.position += new Vector3(0f, 0f, .2f);
+        if (Input.GetKey(KeyCode.A))
+            this.gameObject.transform.position += new Vector3(-.2f, 0f, 0f);
+        if (Input.GetKey(KeyCode.S))
+            this.gameObject.transform.position += new Vector3(0f, 0f, -.2f);
+        if (Input.GetKey(KeyCode.D))
+            this.gameObject.transform.position += new Vector3(.2f, 0f, 0f); ;
+
+        old = cur;
+    }
+}
