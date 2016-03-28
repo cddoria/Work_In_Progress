51fb26fb2add402ef7b23a91d9391b307fc968a4
Maxwell Warren <mwarre28@uncc.edu>
2016-03-28 14:00:42 -0400

    Added Star Collision Testing

---
+++ NEW GAME/Work In Progress/Assets/HealthScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour {
+
+    public int health = 20;
+
+    void OnCollisionEnter(Collision obj)
+    {
+        TakeDamage(obj.gameObject.tag);
+        CheckForDeath();
+    }
+
+    void TakeDamage(string type)
+    {
+        switch(type)
+        {
+            case "Projectile":
+                health -= 5;
+                break;
+            case "Weapon":
+                health -= 10;
+                break;
+        }
+    }
+
+    void CheckForDeath()
+    {
+        if (health <= 0) Destroy(this.gameObject);
+    }
+}
---
+++ NEW GAME/Work In Progress/Assets/NinjaStarScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NinjaStarScript : MonoBehaviour {
+
+    private Rigidbody rb;
+    private Animation anim;
+    private Transform trans;
+    private Collider col, pCol;
+
+    void Start()
+    {
+        rb = gameObject.GetComponentInParent<Rigidbody>();
+        anim = gameObject.GetComponent<Animation>();
+        trans = gameObject.GetComponent<Transform>();
+        col = gameObject.GetComponent<Collider>();
+        pCol = gameObject.GetComponentInParent<Collider>();
+        Physics.IgnoreCollision(col, pCol);
+    }
+
+    // Use this for initialization
+    void OnCollisionEnter(Collision obj)
+    {
+        trans.parent = obj.gameObject.transform;
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+        col.isTrigger = true;
+        anim.Stop();
+    }
+}
